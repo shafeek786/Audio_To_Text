b27407f073ee49f8dab60e2b759b99b72eea35e6
remith-r <remith.r@datamateindia.com>
2025-04-28 22:01:23 +0530

    id fixed

---
+++ AudioToText.Entities/SubDomains/Audio/Modles/DTO/TranscriptionResponse.cs
@@ -0,0 +1,12 @@
+﻿namespace AudioToText.Entities.SubDomains.Audio.Modles.DTO;
+
+public class TranscriptionResponse
+{
+    public long id { get; set; }
+    public string FileName { get; set; } = string.Empty;
+    public long Size { get; set; }
+    public string WebhookUrl { get; set; } = string.Empty;
+    public long TenantId { get; set; }
+    public string TranscriptionType { get; set; } = string.Empty;
+
+}
